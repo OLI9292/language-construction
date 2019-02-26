47d24fdb64a96002f12a3c7e1a7789bbd3b710c1
Oliver Plunkett <otplunkett@gmail.com>
2019-02-26 14:10:09 -0500

    ch

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
 module.exports = {
-  API_URL: process.env.API_URL || "http://localhost:5000/graphql"
+  API_URL: process.env.REACT_APP_API_URL || "http://localhost:5000/graphql"
 }
